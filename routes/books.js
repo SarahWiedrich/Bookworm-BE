e1b1ba7cab000b84f93e58fa388098a64a2f0db6
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-09-30 13:55:53 -0500

    created book routes - all work in postman

---
+++ routes/books.js
@@ -0,0 +1,25 @@
+const { model } = require('mongoose')
+
+const router = require('express').Router()
+
+//GET all books
+router.get('/all', (req, res) => {
+    res.json({message: 'GET all books'})
+})
+
+//GET single book
+router.get('/:id', (req, res) => {
+    res.json({message:'GET single book'})
+})
+
+//POST add book to favorites
+router.post('/add', (req, res) => {
+    res.json({message: 'POST favorite book'})
+})
+
+//DELETE a book from favorites
+router.delete('/:id', (req, res) => {
+    res.json({message: 'DELETE a favorite'})
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const bookroutes = require('./routes/books')
+
 //express app
 const app = express()
 
@@ -10,9 +12,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res)=> {
-    res.send("Hello")
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+//attach routes to app
+app.use('/books',bookroutes)
 
 const PORT = process.env.PORT
 
